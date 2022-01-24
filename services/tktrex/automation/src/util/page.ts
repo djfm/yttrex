d4d2d4054b6425c3ad1de6bb8e88514f3a193bca
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-01-24 20:14:48 +0100

    improvement: factorization

--- services/tktrex/automation/src/util/page.ts
+++ services/tktrex/automation/src/util/page.ts
@@ -2,7 +2,7 @@ import puppeteerVanilla, { Page, Dialog } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import stealth from 'puppeteer-extra-plugin-stealth';
 
-import { prompt, sleep } from './util';
+import { prompt, sleep } from './general';
 
 puppeteer.use(stealth());
 
