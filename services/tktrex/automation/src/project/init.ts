5d22aa1859fedd1b418fd3a3e926d04c044f62e5
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-01-24 21:06:33 +0100

    chore: small fixes

--- services/tktrex/automation/src/project/init.ts
+++ services/tktrex/automation/src/project/init.ts
@@ -1,19 +1,16 @@
-/* eslint-disable no-console */
+import { mkdir } from 'fs/promises';
 
-import {
-  mkdir,
-} from 'fs/promises';
+import { isEmptyDirectoryOrDoesNotExist, shellEscape } from '../util/general';
 
-import {
-  isEmptyDirectoryOrDoesNotExist,
-  shellEscape,
-} from '../util/general';
+import { createLogger } from '../util/logger';
 
 import initTikTokProject from '../platform/TikTok/project';
 
 export const experimentTypes = ['tt-french-elections'] as const;
 
 export type ExperimentType = typeof experimentTypes[number];
+
+const logger = createLogger();
 
 export interface initOptions {
   directory: string;
@@ -24,16 +21,19 @@ export const init = async({
   directory,
   experimentType,
 }: initOptions): Promise<void> => {
-  console.log(`Initializing "${experimentType}" experiment in "${directory}"...`);
+  logger.log(
+    `Initializing "${experimentType}" experiment in "${directory}"...`,
+  );
 
   const ok = await isEmptyDirectoryOrDoesNotExist(directory);
 
   if (ok !== true) {
-    const msg = ok === 'directory-not-empty'
-      ? 'the directory is not empty'
-      : 'the provided path is not a directory';
+    const msg =
+      ok === 'directory-not-empty'
+        ? 'the directory is not empty'
+        : 'the provided path is not a directory';
 
-    console.log(`..failed: ${msg}.`);
+    logger.log(`..failed: ${msg}.`);
     throw new Error(`${msg}: "${directory}"`);
   }
 
@@ -50,9 +50,8 @@ export const init = async({
     throw new Error(`unknown experiment type: "${experimentType}"`);
   }
 
-  console.log(`...done initializing "${experimentType}" project!`);
-  console.log([
-    '',
+  logger.log(`...done initializing "${experimentType}" project!`);
+  logger.log(
     'You can customize the experiment\'s settings by browsing the project\'s directory',
     'and editing the files it contains. A README.md file is also provided to help you.',
     '',
@@ -60,7 +59,7 @@ export const init = async({
     'just execute the following command:',
     '',
     `yarn automate run ${shellEscape(directory)}`,
-  ].join('\n'));
+  );
 };
 
 export default init;
