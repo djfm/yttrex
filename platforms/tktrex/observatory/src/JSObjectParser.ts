c5ed831519cf7fb69c3ca57d9df8c52b55558c53
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-03-03 20:44:05 +0100

    feat: nothing yet

--- platforms/tktrex/observatory/src/JSObjectParser.ts
+++
@@ -1,8 +0,0 @@
-type JSRecord = string | number | boolean | JSRecord[] | JSRecordMap;
-type JSRecordMap = { [key: string]: JSRecord };
-
-export const JSObjectParser = (str: string): JSRecordMap => {
-
-};
-
-export default JSObjectParser;
